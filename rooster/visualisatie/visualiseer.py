54497eb35efe307e730d99c5e80502a7fa1dd857
Joah Goedhart IV <123642414+JoahG2002@users.noreply.github.com>
2025-01-28 00:39:26 +0100

    Add files via upload

--- rooster/visualisatie/visualiseer.py
+++ rooster/visualisatie/visualiseer.py
@@ -110,3 +110,76 @@ def plot_duur_prestatieratios_algoritmen(prestatiedata_algoritme: DataFrame) -> 
             )
         )
         fig.show()
+
+
+def maak_frequentieklassen(prestatiedata_algoritme: DataFrame, doelalgoritme: str) -> dict[str, int]:
+    """
+    Ontvangt een dataframe algoritmeprestaties, en geeft het aantal strafpunten van een doelalgoritmen terug — opgedeeld in frequentieklassen.
+    """
+    frequentieklassen_strafpunten: dict[str, int] = {
+        "< 100": 0, "100–300": 0, "300–500": 0, "500–750": 0, "750–1.000": 0, "1.000–1.500": 0, "1.500–2.000": 0,
+        "2.000–3.000": 0, "3.000–4.000": 0, "4.000–8.000": 0, "8.000–15.000": 0, "15.000–30.000": 0,  "30.000–50.000": 0,
+        "> 50.000": 0,
+    }
+
+    for i in range(prestatiedata_algoritme.__len__()):
+        if not (prestatiedata_algoritme["naamAlgoritme"][i] == doelalgoritme):
+            continue
+
+        aantal_strafpunten_run: int = prestatiedata_algoritme["totaal"][i]
+
+        if aantal_strafpunten_run < 100:
+            frequentieklassen_strafpunten["< 100"] += 1
+        elif aantal_strafpunten_run < 300:
+            frequentieklassen_strafpunten["100–300"] += 1
+        elif aantal_strafpunten_run < 500:
+            frequentieklassen_strafpunten["300–500"] += 1
+        elif aantal_strafpunten_run < 750:
+            frequentieklassen_strafpunten["500–750"] += 1
+        elif aantal_strafpunten_run < 1_000:
+            frequentieklassen_strafpunten["750–1.000"] += 1
+        elif aantal_strafpunten_run < 1_500:
+            frequentieklassen_strafpunten["1.000–1.500"] += 1
+        elif aantal_strafpunten_run < 2_000:
+            frequentieklassen_strafpunten["1.500–2.000"] += 1
+        elif aantal_strafpunten_run < 3_000:
+            frequentieklassen_strafpunten["2.000–3.000"] += 1
+        elif aantal_strafpunten_run < 4_000:
+            frequentieklassen_strafpunten["3.000–4.000"] += 1
+        elif aantal_strafpunten_run < 8_000:
+            frequentieklassen_strafpunten["4.000–8.000"] += 1
+        elif aantal_strafpunten_run < 15_000:
+            frequentieklassen_strafpunten["8.000–15.000"] += 1
+        elif aantal_strafpunten_run < 30_000:
+            frequentieklassen_strafpunten["15.000–30.000"] += 1
+        elif aantal_strafpunten_run < 50_000:
+            frequentieklassen_strafpunten["30.000–50.000"] += 1
+        else:
+            frequentieklassen_strafpunten["> 50.000"] += 1
+
+    return frequentieklassen_strafpunten
+
+
+def plot_verdeling_data(sleutels_met_waarden: dict[str, int | float],
+                        titel_grafiek: str,
+                        titel_x_as: str,
+                        titel_y_as: str,
+                        kleur_balken_grafiek: str,
+                        gesorteerd: bool = False) -> None:
+    """
+    Plot de frequentie van een tijdeenheid van de geboortedata.
+    """
+    if gesorteerd:
+        sleutels_met_waarden = dict(sorted(sleutels_met_waarden.items(), key=lambda x: x[1]))
+
+    fig: go.Figure = go.Figure(
+        data=go.Bar(
+            x=list(sleutels_met_waarden.keys()),
+            y=list(sleutels_met_waarden.values()),
+            marker_color=kleur_balken_grafiek
+        )
+    )
+
+    fig.update_layout(title=titel_grafiek, xaxis_title=titel_x_as, yaxis_title=titel_y_as)
+    fig.update_xaxes(type="category")
+    fig.show()
